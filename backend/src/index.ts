bcb08c3d9609d39899e00bbc650a23d688387cf7
prayudahlah <yudafihan@gmail.com>
2026-05-21 23:13:43 +0700

    feat: add POST /upload endpoint
    - takes in binary file
    - response with success flag and object_key
    - also added /openapi for api documentation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,35 @@
 import { Elysia } from "elysia";
+import { uploadChunk } from './services/upload';
+import { openapi, fromTypes } from '@elysia/openapi'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+    .use(
+        openapi({
+            references: fromTypes(
+                process.env.NODE_ENV === 'production'
+                    ? 'dist/index.d.ts'
+                    : 'src/index.ts'
+            )
+        })
+    )
+
+app.get("/", () => "Service Healthy. Milik Kelompok 5 KDA :)").listen(3000);
+
+app.post('/upload', async ({ request }) => {
+    try {
+        const blob = await request.blob();
+        const object_key = await uploadChunk(blob);
+        return { success: true, object_key };
+    } catch (err: any) {
+        return {
+            success: false,
+            error: err.message || 'Upload failed',
+            details: err.code || 'Unknown'
+        };
+
+    }
+}).listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
