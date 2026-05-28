1af92bdb90bc8f55b2e4a1f5160efe41d7758a3f
anin <aanindya05@gmail.com>
2026-05-28 08:52:45 +0700

    feat(backend): tambah CORS dan perbaiki chaining Elysia

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 import { uploadChunk } from './services/upload';
 import { openapi, fromTypes } from '@elysia/openapi'
 
 const app = new Elysia()
+    .use(cors())
     .use(
         openapi({
             references: fromTypes(
@@ -12,23 +14,21 @@ const app = new Elysia()
             )
         })
     )
-
-app.get("/", () => "Service Healthy. Milik Kelompok 5 KDA :)").listen(3000);
-
-app.post('/upload', async ({ request }) => {
-    try {
-        const blob = await request.blob();
-        const object_key = await uploadChunk(blob);
-        return { success: true, object_key };
-    } catch (err: any) {
-        return {
-            success: false,
-            error: err.message || 'Upload failed',
-            details: err.code || 'Unknown'
-        };
-
-    }
-}).listen(3000);
+    .get("/", () => "Service Healthy. Milik Kelompok 5 KDA :)")
+    .post('/upload', async ({ request }) => {
+        try {
+            const blob = await request.blob();
+            const object_key = await uploadChunk(blob);
+            return { success: true, object_key };
+        } catch (err: any) {
+            return {
+                success: false,
+                error: err.message || 'Upload failed',
+                details: err.code || 'Unknown'
+            };
+        }
+    })
+    .listen(3000);
 
 console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
